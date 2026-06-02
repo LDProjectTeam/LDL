5ad29a3f92485a61aeb04a76249791ab3f609ffc
LD PROJECT <ldprojectteams@gmail.com>
2026-06-02 23:40:03 +0300

    Delete frontend directory

--- frontend/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Window {
-    electronAPI?: {
-        closeWindow: () => void;
-        openExternal: (url: string) => void;
-        [key: string]: any;
-    };
-}
